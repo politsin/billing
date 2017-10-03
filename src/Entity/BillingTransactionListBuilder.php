a24f5384f45a54236c0a5365daffe214b6c0f003
Anatoly Politsin <politsin@gmail.com>
2017-10-03 18:35:40 +0300

    Transaction List

--- src/Entity/BillingTransactionListBuilder.php
+++ src/Entity/BillingTransactionListBuilder.php
@@ -16,9 +16,50 @@ class BillingTransactionListBuilder extends EntityListBuilder {
   /**
    * {@inheritdoc}
    */
+  public function render() {
+    $build['table'] = [
+      '#type' => 'table',
+      '#header' => $this->buildHeader(),
+      '#title' => $this->getTitle(),
+      '#rows' => [],
+      '#empty' => $this->t('There is no @label yet.', ['@label' => $this->entityType->getLabel()]),
+      '#cache' => [
+        'contexts' => $this->entityType->getListCacheContexts(),
+        'tags' => $this->entityType->getListCacheTags(),
+      ],
+    ];
+    foreach ($this->load() as $entity) {
+      if ($row = $this->buildRow($entity)) {
+        $build['table']['#rows'][$entity->id()] = $row;
+      }
+    }
+
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $build['pager'] = [
+        '#type' => 'pager',
+      ];
+    }
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function buildHeader() {
-    $header['id'] = $this->t('Billing transaction ID');
-    $header['name'] = $this->t('Name');
+    $header['id'] = $this->t('ID');
+    $header['name'] = $this->t('Comment');
+    $header['account'] = $this->t('Account');
+    $header['reason'] = $this->t('Reason');
+    $header['debit'] = [
+      'class' => 'text-align-right',
+      'data' => $this->t('Debit'),
+    ];
+    $header['credit'] = [
+      'class' => 'text-align-right',
+      'data' => $this->t('Credit'),
+    ];
+    $header['user'] = $this->t('User');
     return $header + parent::buildHeader();
   }
 
@@ -30,8 +71,27 @@ class BillingTransactionListBuilder extends EntityListBuilder {
     $row['id'] = $entity->id();
     $row['name'] = Link::createFromRoute(
       $entity->label(),
-      'entity.billing_transaction.edit_form',
+      'entity.billing_transaction.canonical',
       ['billing_transaction' => $entity->id()]
+    );
+    $row['account'] = Link::createFromRoute(
+      $entity->account_id->entity->label(),
+      'entity.billing_account.canonical',
+      ['billing_account' => $entity->account_id->entity->id()]
+    );
+    $row['reason'] = $entity->entity_type->value . "-" . $entity->entity_id->value;
+    $row['debit'] = [
+      'class' => 'text-align-right',
+      'data' => $entity->debit->value,
+    ];
+    $row['credit'] = [
+      'class' => 'text-align-right',
+      'data' => $entity->credit->value,
+    ];
+    $row['user_id'] = Link::createFromRoute(
+      $entity->getOwner()->label(),
+      'entity.user.canonical',
+      ['user' => $entity->getOwner()->id()]
     );
     return $row + parent::buildRow($entity);
   }
